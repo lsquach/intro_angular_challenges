5a08e8f35b4ac4ff98512af047a5c63d5bcef6a0
lsquach <lsquach@gmail.com>
2016-04-13 14:40:09 -0700

    finished sprint 1

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,16 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("WdiController", WdiController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Lotus";
+  this.shout = function (str){
+    return str.toUpperCase() + "!";
+  };
+}
+
+function WdiController () {
+  this.class_name = "Web Dev Immersive";
+}
